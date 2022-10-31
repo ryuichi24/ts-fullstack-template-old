78bfbd0fe4f2fa72bda4ed91e7ce5e10fb2519e3
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-10-31 07:26:35 +0000

    add lint and format scripts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,24 +2,22 @@ import express from "express";
 import cors from "cors";
 
 async function main() {
-  const PORT = process.env.PORT || 5555;
-  const HOST = process.env.HOST || "localhost";
-  const app = express();
+    const PORT = process.env.PORT || 5555;
+    const HOST = process.env.HOST || "localhost";
+    const app = express();
 
-  app.use(
-    cors({
-      origin: ["http://127.0.0.1:3333"],
-      credentials: true,
-    })
-  );
+    app.use(
+        cors({
+            origin: ["http://127.0.0.1:3333"],
+            credentials: true,
+        })
+    );
 
-  app.get("*", (_, res) => {
-    res.status(200).send("server is up and running");
-  });
+    app.get("*", (_, res) => {
+        res.status(200).send("server is up and running");
+    });
 
-  app.listen(PORT, () =>
-    console.log(`server is up and running at http://${HOST}:${PORT}`)
-  );
+    app.listen(PORT, () => console.log(`server is up and running at http://${HOST}:${PORT}`));
 }
 
-main().catch(err => console.error(err));
+main().catch((err) => console.error(err));
