7fa861e7107b05636878282eac366eba6df2d712
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-05 13:34:05 +0900

    minor fix

--- packages/server/.eslintrc.cjs
+++ packages/server/.eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
         ecmaVersion: "latest",
         sourceType: "module",
         tsconfigRootDir: __dirname,
-        project: ["tsconfig.config.json"],
+        project: ["tsconfig.eslint.json"],
     },
     plugins: ["@typescript-eslint"],
     ignorePatterns: ["**/__generated__/*ts"],
