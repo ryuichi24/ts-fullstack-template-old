68ed09b150f69f760c0c5783560a1e4d272d1776
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-06 06:08:45 +0000

    gen gql in web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,32 +1,8 @@
-import { useEffect } from "react";
-import { useBooksQuery } from "./__generated__/graphql";
-
 function App() {
-    const { data, loading, error } = useBooksQuery({
-        variables: {},
-    });
-
-    useEffect(() => {
-        fetch("http://localhost:5555")
-            .then((res) => res.text())
-            .then((text) => console.log(text))
-            .catch((err) => console.log(err));
-    }, []);
-
-    if (loading) {
-        return <div>loading...</div>;
-    }
-
-    if (!loading && !data) {
-        return <div>React App</div>;
-    }
-
     return (
-        <div>
-            {data?.books?.map((book) => (
-                <div key={book?.title}>{book?.title}</div>
-            ))}
-        </div>
+        <>
+            <div>React App</div>
+        </>
     );
 }
 
--- packages/web/src/__generated__/graphql.tsx
+++ packages/web/src/__generated__/graphql.tsx
@@ -1,5 +1,5 @@
-import { gql } from "@apollo/client";
-import * as Apollo from "@apollo/client";
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -8,71 +8,260 @@ export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Mayb
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
-export type Book = {
-    __typename?: "Book";
-    author?: Maybe<Scalars["String"]>;
-    title?: Maybe<Scalars["String"]>;
+export type CheckAuthResponse = {
+  __typename?: 'CheckAuthResponse';
+  user?: Maybe<User>;
+};
+
+export type ConfirmEmailInput = {
+  token: Scalars['String'];
+};
+
+export type ConfirmEmailResponse = {
+  __typename?: 'ConfirmEmailResponse';
+  user?: Maybe<User>;
+};
+
+export type LoginInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type LoginResponse = {
+  __typename?: 'LoginResponse';
+  user?: Maybe<User>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  confirmEmail?: Maybe<ConfirmEmailResponse>;
+  login?: Maybe<LoginResponse>;
+  register?: Maybe<RegisterResponse>;
+};
+
+
+export type MutationConfirmEmailArgs = {
+  confirmEmailInput: ConfirmEmailInput;
+};
+
+
+export type MutationLoginArgs = {
+  loginInput: LoginInput;
+};
+
+
+export type MutationRegisterArgs = {
+  registerInput: RegisterInput;
 };
 
 export type Query = {
-    __typename?: "Query";
-    books?: Maybe<Array<Maybe<Book>>>;
+  __typename?: 'Query';
+  checkAuth?: Maybe<CheckAuthResponse>;
+  health?: Maybe<Scalars['Boolean']>;
 };
 
-export type BooksQueryVariables = Exact<{ [key: string]: never }>;
-
-export type BooksQuery = {
-    __typename?: "Query";
-    books?: Array<{
-        __typename?: "Book";
-        title?: string | null;
-        author?: string | null;
-    } | null> | null;
+export type RegisterInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
 
-export const BooksDocument = gql`
-    query Books {
-        books {
-            title
-            author
-        }
+export type RegisterResponse = {
+  __typename?: 'RegisterResponse';
+  user?: Maybe<User>;
+};
+
+export type User = {
+  __typename?: 'User';
+  createdAt?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars['String']>;
+  updatedAt?: Maybe<Scalars['String']>;
+};
+
+export type ConfirmEmailMutationVariables = Exact<{
+  confirmEmailInput: ConfirmEmailInput;
+}>;
+
+
+export type ConfirmEmailMutation = { __typename?: 'Mutation', confirmEmail?: { __typename?: 'ConfirmEmailResponse', user?: { __typename?: 'User', id?: string | null, email?: string | null, createdAt?: string | null, updatedAt?: string | null } | null } | null };
+
+export type LoginMutationVariables = Exact<{
+  loginInput: LoginInput;
+}>;
+
+
+export type LoginMutation = { __typename?: 'Mutation', login?: { __typename?: 'LoginResponse', user?: { __typename?: 'User', id?: string | null, email?: string | null, createdAt?: string | null, updatedAt?: string | null } | null } | null };
+
+export type RegisterMutationVariables = Exact<{
+  registerInput: RegisterInput;
+}>;
+
+
+export type RegisterMutation = { __typename?: 'Mutation', register?: { __typename?: 'RegisterResponse', user?: { __typename?: 'User', id?: string | null, email?: string | null, createdAt?: string | null, updatedAt?: string | null } | null } | null };
+
+export type CheckAuthQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type CheckAuthQuery = { __typename?: 'Query', checkAuth?: { __typename?: 'CheckAuthResponse', user?: { __typename?: 'User', id?: string | null, email?: string | null, updatedAt?: string | null, createdAt?: string | null } | null } | null };
+
+
+export const ConfirmEmailDocument = gql`
+    mutation ConfirmEmail($confirmEmailInput: ConfirmEmailInput!) {
+  confirmEmail(confirmEmailInput: $confirmEmailInput) {
+    user {
+      id
+      email
+      createdAt
+      updatedAt
     }
-`;
+  }
+}
+    `;
+export type ConfirmEmailMutationFn = Apollo.MutationFunction<ConfirmEmailMutation, ConfirmEmailMutationVariables>;
 
 /**
- * __useBooksQuery__
+ * __useConfirmEmailMutation__
  *
- * To run a query within a React component, call `useBooksQuery` and pass it any options that fit your needs.
- * When your component renders, `useBooksQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a mutation, you first call `useConfirmEmailMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useConfirmEmailMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [confirmEmailMutation, { data, loading, error }] = useConfirmEmailMutation({
+ *   variables: {
+ *      confirmEmailInput: // value for 'confirmEmailInput'
+ *   },
+ * });
+ */
+export function useConfirmEmailMutation(baseOptions?: Apollo.MutationHookOptions<ConfirmEmailMutation, ConfirmEmailMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<ConfirmEmailMutation, ConfirmEmailMutationVariables>(ConfirmEmailDocument, options);
+      }
+export type ConfirmEmailMutationHookResult = ReturnType<typeof useConfirmEmailMutation>;
+export type ConfirmEmailMutationResult = Apollo.MutationResult<ConfirmEmailMutation>;
+export type ConfirmEmailMutationOptions = Apollo.BaseMutationOptions<ConfirmEmailMutation, ConfirmEmailMutationVariables>;
+export const LoginDocument = gql`
+    mutation Login($loginInput: LoginInput!) {
+  login(loginInput: $loginInput) {
+    user {
+      id
+      email
+      createdAt
+      updatedAt
+    }
+  }
+}
+    `;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
+
+/**
+ * __useLoginMutation__
+ *
+ * To run a mutation, you first call `useLoginMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLoginMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [loginMutation, { data, loading, error }] = useLoginMutation({
+ *   variables: {
+ *      loginInput: // value for 'loginInput'
+ *   },
+ * });
+ */
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
+      }
+export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
+export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const RegisterDocument = gql`
+    mutation Register($registerInput: RegisterInput!) {
+  register(registerInput: $registerInput) {
+    user {
+      id
+      email
+      createdAt
+      updatedAt
+    }
+  }
+}
+    `;
+export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      registerInput: // value for 'registerInput'
+ *   },
+ * });
+ */
+export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
+      }
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
+export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
+export const CheckAuthDocument = gql`
+    query CheckAuth {
+  checkAuth {
+    user {
+      id
+      email
+      updatedAt
+      createdAt
+    }
+  }
+}
+    `;
+
+/**
+ * __useCheckAuthQuery__
+ *
+ * To run a query within a React component, call `useCheckAuthQuery` and pass it any options that fit your needs.
+ * When your component renders, `useCheckAuthQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useBooksQuery({
+ * const { data, loading, error } = useCheckAuthQuery({
  *   variables: {
  *   },
  * });
  */
-export function useBooksQuery(
-    baseOptions?: Apollo.QueryHookOptions<BooksQuery, BooksQueryVariables>
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useQuery<BooksQuery, BooksQueryVariables>(BooksDocument, options);
-}
-export function useBooksLazyQuery(
-    baseOptions?: Apollo.LazyQueryHookOptions<BooksQuery, BooksQueryVariables>
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useLazyQuery<BooksQuery, BooksQueryVariables>(BooksDocument, options);
-}
-export type BooksQueryHookResult = ReturnType<typeof useBooksQuery>;
-export type BooksLazyQueryHookResult = ReturnType<typeof useBooksLazyQuery>;
-export type BooksQueryResult = Apollo.QueryResult<BooksQuery, BooksQueryVariables>;
+export function useCheckAuthQuery(baseOptions?: Apollo.QueryHookOptions<CheckAuthQuery, CheckAuthQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<CheckAuthQuery, CheckAuthQueryVariables>(CheckAuthDocument, options);
+      }
+export function useCheckAuthLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<CheckAuthQuery, CheckAuthQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<CheckAuthQuery, CheckAuthQueryVariables>(CheckAuthDocument, options);
+        }
+export type CheckAuthQueryHookResult = ReturnType<typeof useCheckAuthQuery>;
+export type CheckAuthLazyQueryHookResult = ReturnType<typeof useCheckAuthLazyQuery>;
+export type CheckAuthQueryResult = Apollo.QueryResult<CheckAuthQuery, CheckAuthQueryVariables>;
