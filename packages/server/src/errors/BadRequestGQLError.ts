03905fc72cc77fe9a8e314ea51a8c2bcf93ac69b
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-07 10:31:45 +0000

    minor

--- packages/server/src/errors/BadRequestGQLError.ts
+++ packages/server/src/errors/BadRequestGQLError.ts
@@ -1,13 +1,15 @@
 import { CustomGQLError } from "./CustomGQLError.js";
+import { ApolloServerErrorCode } from "@apollo/server/errors";
 
 export class BadRequestGQLError extends CustomGQLError {
     constructor(message: string, field?: string) {
         super(message, field, {
             extensions: {
-                code: "BAD_REQUEST",
+                code: ApolloServerErrorCode.BAD_USER_INPUT,
                 http: { status: 400 },
                 field,
             },
         });
     }
 }
+// https://www.apollographql.com/docs/apollo-server/data/errors/
