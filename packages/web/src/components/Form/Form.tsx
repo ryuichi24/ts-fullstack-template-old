a889f4e8dc7d042e485dbc670172634c7345da2a
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-09 09:24:25 +0000

    minor

--- packages/web/src/components/Form/Form.tsx
+++ packages/web/src/components/Form/Form.tsx
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 
 // https://qiita.com/yuma84/items/72f72a2fff987d3de28a
-type FieldErrorMessages<TFormInputs, TFieldKeys extends keyof TFormInputs> = {
-    [key in TFieldKeys]: string;
+type FieldErrorMessages<TFormInputs, TFormInputKey extends keyof TFormInputs> = {
+    [key in TFormInputKey]: string;
 };
 
-type FormProps<TFormInputs, TFieldKeys extends keyof TFormInputs> = {
+type FormProps<TFormInputs, TFormInputKey extends keyof TFormInputs> = {
     onSubmit: (args: {
         values: TFormInputs;
         setFieldErrorMessages: React.Dispatch<
-            React.SetStateAction<FieldErrorMessages<TFormInputs, TFieldKeys>>
+            React.SetStateAction<FieldErrorMessages<TFormInputs, TFormInputKey>>
         >;
     }) => Promise<void>;
     children: (args: {
-        fieldErrorMessages: FieldErrorMessages<TFormInputs, TFieldKeys>;
+        fieldErrorMessages: FieldErrorMessages<TFormInputs, TFormInputKey>;
         handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     }) => React.ReactNode;
     className?: string;
 };
 
-export const Form = <TFormInputs extends {}, TFieldKeys extends keyof TFormInputs>({
+export const Form = <TFormInputs extends {}, TFormInputKey extends keyof TFormInputs>({
     onSubmit,
     children,
     className = "",
-}: FormProps<TFormInputs, TFieldKeys>) => {
+}: FormProps<TFormInputs, TFormInputKey>) => {
     const [formInputs, setFormInputs] = useState<TFormInputs>({} as TFormInputs);
     const [fieldErrorMessages, setFieldErrorMessages] = useState<
-        FieldErrorMessages<TFormInputs, TFieldKeys>
-    >({} as FieldErrorMessages<TFormInputs, TFieldKeys>);
+        FieldErrorMessages<TFormInputs, TFormInputKey>
+    >({} as FieldErrorMessages<TFormInputs, TFormInputKey>);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const targetFieldName = event.target.name as TFieldKeys;
-        const targetValue = event.target.value as TFormInputs[TFieldKeys];
+        const targetFieldName = event.target.name as TFormInputKey;
+        const targetValue = event.target.value as TFormInputs[TFormInputKey];
         setFormInputs((prev) => {
             const nextFormInputs: TFormInputs = { ...prev };
             nextFormInputs[targetFieldName] = targetValue;
