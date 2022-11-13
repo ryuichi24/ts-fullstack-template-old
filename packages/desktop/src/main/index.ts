d2f55c2fc974cbd096191682a1bc671e70562106
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-13 16:54:20 +0900

    format

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import { BrowserWindow, app } from "electron";
 
-const isDev = !app.isPackaged
+const isDev = !app.isPackaged;
 const isMac = process.platform === "darwin";
 const RENDERER_PORT = process.env.RENDERER_PORT;
 
@@ -9,42 +9,42 @@ let mainWindow: BrowserWindow | null;
 
 // Main Window
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    width: isDev ? 1000 : 500,
-    height: 600,
-    //   icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: true,
-      preload: path.resolve(__dirname, "preload.js"),
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: isDev ? 1000 : 500,
+        height: 600,
+        //   icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+            preload: path.resolve(__dirname, "preload.js"),
+        },
+    });
 
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-    mainWindow.loadURL(`http://localhost:${RENDERER_PORT}`);
-  } else {
-    mainWindow.loadFile(
-      path.resolve(path.dirname(__dirname), "src", "renderer/dist/index.html")
-    );
-  }
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+        mainWindow.loadURL(`http://localhost:${RENDERER_PORT}`);
+    } else {
+        mainWindow.loadFile(
+            path.resolve(path.dirname(__dirname), "src", "renderer/dist/index.html")
+        );
+    }
 }
 
 app.whenReady().then(() => {
-  createMainWindow();
+    createMainWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createMainWindow();
-    }
-  });
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
 
-  app.on("window-all-closed", () => {
-    if (!isMac) {
-      app.quit();
-    }
-  });
+    app.on("window-all-closed", () => {
+        if (!isMac) {
+            app.quit();
+        }
+    });
 
-  mainWindow?.on("closed", () => (mainWindow = null));
+    mainWindow?.on("closed", () => (mainWindow = null));
 });
