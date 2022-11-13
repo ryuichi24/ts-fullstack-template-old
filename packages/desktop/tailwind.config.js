6ff28639734e6860c382a4e85561f10b2873173d
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-13 16:51:23 +0900

    add tailwind in desktop

---
+++ packages/desktop/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/renderer/index.html", "./src/renderer/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
